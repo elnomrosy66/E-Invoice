0a2051ad5ddfaabd107c7d5630adcc8e69e44a89
elnomrosy66 <elnomrosy66@gmail.com>
2026-09-18 18:04:16 +0300

    Add automatic schema migration for PosDevices table and Order POS columns in ApplicationDBContext

--- E-Invoice.DAL/E_Invoice.DAL.Data/ApplicationDBContext.cs
+++ E-Invoice.DAL/E_Invoice.DAL.Data/ApplicationDBContext.cs
@@ -39,7 +39,92 @@ public class ApplicationDBContext : DbContext
 
 	public DbSet<PosDevice> PosDevices { get; set; }
 
+	private static bool _tablesChecked = false;
+	private static readonly object _checkLock = new object();
+
 	public ApplicationDBContext()
+		: base("name=LocafflDb")
+	{
+		Database.SetInitializer<ApplicationDBContext>(null);
+		EnsureDatabaseSchema();
+	}
+
+	public static void EnsureDatabaseSchema()
+	{
+		if (_tablesChecked) return;
+		lock (_checkLock)
+		{
+			if (_tablesChecked) return;
+			try
+			{
+				using (var ctx = new ApplicationDBContext(true))
+				{
+					string sql = @"
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PosDevices]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE [dbo].[PosDevices] (
+        [Id] INT IDENTITY(1,1) NOT NULL,
+        [Code] NVARCHAR(50) NULL,
+        [Name] NVARCHAR(100) NULL,
+        [PosName] NVARCHAR(100) NOT NULL,
+        [PosCode] NVARCHAR(50) NOT NULL,
+        [BranchId] INT NOT NULL DEFAULT(1),
+        [DeviceSerialNumber] NVARCHAR(100) NULL,
+        [DeviceOSVersion] NVARCHAR(50) NULL,
+        [DeviceModel] NVARCHAR(50) NULL,
+        [ActivityCode] NVARCHAR(50) NULL,
+        [ClientId] NVARCHAR(150) NULL,
+        [ClientSecret] NVARCHAR(150) NULL,
+        [CurrentSequence] BIGINT NOT NULL DEFAULT(1),
+        [ReceiptPrefix] NVARCHAR(20) NOT NULL DEFAULT('REC-'),
+        [IsProduction] BIT NOT NULL DEFAULT(0),
+        [CreatedDate] DATETIME NULL,
+        [CreatedBy] INT NULL,
+        [IsDelete] INT NOT NULL DEFAULT(0),
+        [IsActive] BIT NOT NULL DEFAULT(1),
+        CONSTRAINT [PK_dbo.PosDevices] PRIMARY KEY CLUSTERED ([Id] ASC)
+    );
+END
+
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Orders]') AND name = 'PosDeviceId')
+BEGIN
+    ALTER TABLE [dbo].[Orders] ADD [PosDeviceId] INT NULL;
+END
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Orders]') AND name = 'ReceiptNumber')
+BEGIN
+    ALTER TABLE [dbo].[Orders] ADD [ReceiptNumber] BIGINT NULL;
+END
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Orders]') AND name = 'FullReceiptNumber')
+BEGIN
+    ALTER TABLE [dbo].[Orders] ADD [FullReceiptNumber] NVARCHAR(100) NULL;
+END
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Orders]') AND name = 'PaymentMethod')
+BEGIN
+    ALTER TABLE [dbo].[Orders] ADD [PaymentMethod] NVARCHAR(50) NULL;
+END
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Orders]') AND name = 'PreviousUUID')
+BEGIN
+    ALTER TABLE [dbo].[Orders] ADD [PreviousUUID] NVARCHAR(150) NULL;
+END
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[Orders]') AND name = 'ReferenceOldUUID')
+BEGIN
+    ALTER TABLE [dbo].[Orders] ADD [ReferenceOldUUID] NVARCHAR(150) NULL;
+END
+";
+					ctx.Database.ExecuteSqlCommand(sql);
+				}
+			}
+			catch
+			{
+			}
+			finally
+			{
+				_tablesChecked = true;
+			}
+		}
+	}
+
+	private ApplicationDBContext(bool internalInit)
 		: base("name=LocafflDb")
 	{
 		Database.SetInitializer<ApplicationDBContext>(null);
